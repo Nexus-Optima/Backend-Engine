f41a180dc81f274736a8707ac4337b4d9113dee4
Ashutosh Srivastava <aniketjsr98@gmail.com>
2024-03-23 10:24:49 +0530

    all the api login has been implemented in user_details folder keeping app.py file clean

--- src/Database/user_details.py
+++ src/Database/user_details.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
+from flask import jsonify
 from src.Utils.constants import Database
 import os
 
@@ -18,7 +19,16 @@ def get_dynamodb_client_table():
     table = dynamodb.Table(Database.client_table)
     return table
 
-def update_client_details(table, userid, username,email, phone, company):
+def update_client_details(table,data):
+    userid = data.get('userid')
+    if not userid:
+        return jsonify({"error": "Invalid input"}), 400
+    username = data.get('username')
+    email = data.get('email')
+    phone = data.get('phone')
+    company = data.get('company')
+    if not userid or not username or not phone or not company:
+        return jsonify({"error": "Invalid input. Please provide userid, username, email, phone, and company."}), 400
     try:
         item = {
             'userid': userid,
@@ -32,6 +42,54 @@ def update_client_details(table, userid, username,email, phone, company):
     except ClientError as e:
         error_message = e.response['Error']['Message']
         return False, error_message
+
+
+def get_user_details(email, table):
+    try:
+        response = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('email').eq(email)
+        )
+        items = response.get('Items', [])
+        print(items)
+        return items
+
+    except NoCredentialsError:
+        raise Exception("Error: AWS credentials not found. Make sure to set up your credentials.")
+    except Exception as e:
+        raise Exception(f"Error in get_user_data_from_database: {e}")
+
+
+def update_personal_details(table, data):
+    email = data.get('email')
+    if not email:
+        return jsonify({"error": "Invalid input"}), 400
+    username = data.get('username')
+    userid = data.get('userid')
+    phone = data.get('phone')
+    company = data.get('company')
+    if not username or not phone or not company:
+        return jsonify({"error": "Invalid input. Please provide userid, username, email, phone, and company."}), 400
+
+    try:
+        response=table.update_item(
+            Key={
+                'email': email
+            },
+            UpdateExpression='set userid = :userid, username = :username, phone = :phone, company = :company',
+            ExpressionAttributeValues={
+                ':userid': userid,
+                ':username': username,
+                ':phone': phone,
+                ':company': company
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        updated_user=response.get('Attributes',[])
+        return True, updated_user
+    except Exception as e:
+        return False, str(e)
+
+
 
 
 
--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import logging
 from dotenv import load_dotenv
-from Database.user_details import get_dynamodb_client_table, update_client_details
+from Database.user_details import get_dynamodb_client_table, update_client_details, get_user_details,update_personal_details
 from Database.database import get_dynamodb_table, update_module_data, get_user_data
 
 app = Flask(__name__)
@@ -72,28 +72,39 @@ def update_user():
 @app.route('/update_details', methods=['POST'])
 def update_details():
     data = request.json
-    print(data)
-    userid = data.get('userid')
-    if not userid:
-        return jsonify({"error": "Invalid input"}), 400
-    username = data.get('username')
-    email = data.get('email')
-    phone = data.get('phone')
-    company = data.get('company')
-    if not userid or not username or not phone or not company:
-        return jsonify({"error": "Invalid input. Please provide userid, username, email, phone, and company."}), 400
-
-    table = get_dynamodb_client_table()
-    success, error_message = update_client_details(table, userid, username,email, phone, company)
+    success, error_message = update_client_details(client_table, data)
 
     if success:
         return jsonify({"success": True}), 200
     else:
         return jsonify({"error": "Failed to update client details.", "message": error_message}), 500
 
-    #update_client_details(table, userid,username, phone, company)
 
+@app.route('/get_details', methods=['GET'])
+def get():
+    try:
+        email = request.args.get('email')
+        if not email:
+            return jsonify({"error": "Missing 'cid' parameter"}), 400
+        user_details = get_user_details(email, client_table)
 
+        if user_details is not None:
+            return jsonify(user_details)
+        else:
+            return jsonify({"error": "User not found"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/update_personal_details/<string:email>', methods=['PUT'])
+def update_personal_detail(email):
+    data = request.json
+
+    success, error_message = update_personal_details(client_table, data)
+    if success:
+        return jsonify({"success": True}), 200
+    else:
+        return jsonify({"error": "Failed to update client details.", "message": error_message}), 500
 
 
 if __name__ == '__main__':
